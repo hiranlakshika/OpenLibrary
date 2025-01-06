e766905a98c3c688bc2423726a3d10f4c10ec396
Hiran Lakshika <hiranlakshika@gmail.com>
2025-01-06 20:04:46 +0100

    Local book change event

--- composeApp/src/commonMain/kotlin/com/restable/library/book/presentation/book_list/BookListViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/restable/library/book/presentation/book_list/BookListViewModel.kt
@@ -7,8 +7,13 @@ import com.restable.library.book.domain.usecase.SearchBooksUseCase
 import com.restable.library.core.domain.Result
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.IO
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
@@ -16,13 +21,13 @@ class BookListViewModel(
     private val searchBooksUseCase: SearchBooksUseCase,
     private val getWishListUseCase: GetWishListUseCase,
 ) : ViewModel() {
-
+    private var observeFavoriteJob: Job? = null
     private val _state = MutableStateFlow(BookListState())
-    val state get() = _state
-
-    init {
+    val state = _state.onStart {
         fetchLocalBooks()
-    }
+    }.stateIn(
+        viewModelScope, SharingStarted.WhileSubscribed(5000L), _state.value
+    )
 
     fun onEvent(event: BookListEvent) {
         when (event) {
@@ -68,9 +73,13 @@ class BookListViewModel(
             }
 
             is Result.Success -> {
-                _state.update {
-                    it.copy(isLoading = false, wishlistBooks = result.data.first())
-                }
+                observeFavoriteJob?.cancel()
+                observeFavoriteJob = result.data.onEach { value ->
+                    _state.update {
+                        it.copy(isLoading = false, wishlistBooks = value)
+                    }
+
+                }.launchIn(viewModelScope)
             }
         }
     }
